3299bee8e8dbafd6015447031801c35bfa2403bd
Ray1235 <ray1235@me.com>
2017-01-21 10:19:32 +0100

    usunieto przycisk maksymalizowania

--- PGJ002/Form1.Designer.cs
+++ PGJ002/Form1.Designer.cs
@@ -35,6 +35,7 @@
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
+            this.MaximizeBox = false;
             this.Name = "Form1";
             this.ShowIcon = false;
             this.Text = "Form1";
